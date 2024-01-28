365a89742196cb6a1eff7690efd08221256e2dc1
Artem Tarasenko <shabashab.04@gmail.com>
2024-01-28 16:57:53 +0200

    init commit

---
+++ packages/base/index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "env": {
+    "node": true
+  },
+  "extends": ["standard"],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": ["@typescript-eslint"],
+  "rules": {
+    "no-inline-comments": "error",
+    "no-irregular-whitespace": "error",
+    "no-multi-spaces": "error",
+    "no-multiple-empty-lines": "error",
+    "no-console": "error",
+    "no-control-regex": "warn",
+    "curly": "error",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/prefer-readonly": ["error", { "onlyInlineLambdas": true }],
+    "@typescript-eslint/return-await": ["error", "always"]
+  }
+}
---
+++ packages/vue/index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    '@inc-dev',
+    'plugin:vue/vue3-recommended'
+  ],
+  overrides: [
+    {
+      files: ['*.vue', '*.jsx', '*.tsx'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.vue']
+      }
+    }
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/padding-line-between-blocks': 'error',
+    'no-constant-condition': 'warn',
+    'no-console': 'off'
+  },
+}
